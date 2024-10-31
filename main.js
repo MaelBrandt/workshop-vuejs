3cc4cc60e54c2c20989999951f3adada918f31e0
mael brandt <mael.brandt@he-arc.ch>
2024-10-31 10:31:05 +0100

    v-else-if

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const app = Vue.createApp({
     data: function () {
-
       return {
         title: 'Achat de café Nespresso',
         desc: "L'histoire de NESPRESSO est celle d'une passion inaltérable pour la perfection et d'une quête inlassable d'innovation en faveur de la plus haute qualité.",
         image: 'assets\\images\\colombia.png',
         url: "https://www.nespresso.com/ch/fr/",
-        inStock: false,
+        inStock: true,
+        stock: 5,
       };
     },
   });
