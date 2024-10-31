8ec92a9b43a0447eb491fd6e4ea3af93249190c1
mael brandt <mael.brandt@he-arc.ch>
2024-10-31 09:58:48 +0100

    3. Attribute Binding

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const app = Vue.createApp({
     data: function () {
+
       return {
         title: 'Achat de café Nespresso',
-        desc: 'description',
+        desc: "L'histoire de NESPRESSO est celle d'une passion inaltérable pour la perfection et d'une quête inlassable d'innovation en faveur de la plus haute qualité.",
+        image: 'assets\\images\\colombia.png'
       };
     },
   });
